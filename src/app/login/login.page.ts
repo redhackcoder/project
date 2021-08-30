a820c1c3aea639c8b774b04c58e721d0c9824a03
redhackcoder <vishal.bajpai27@gmail.com>
2021-08-30 23:20:18 +0530

    first commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,24 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { UtilService } from '../util.service';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+  register:any;
+  oset=0;
+  showCity = true;
   constructor(
     private util: UtilService,
     private navCtrl: NavController, 
   ) { }
 
   ngOnInit() {
+    // console.log(registerCredentials)
   }
 
+  otp() {
+    // Enabling Side Menu
+    this.showCity = false;
+    this.oset = 1;
+    // this.util.setMenuState(true);
+    // this.navCtrl.navigateRoot('/home', { animationDirection: 'forward' });
+  }
   login() {
     // Enabling Side Menu
+    this.showCity = false;
     this.util.setMenuState(true);
     this.navCtrl.navigateRoot('/home', { animationDirection: 'forward' });
   }
